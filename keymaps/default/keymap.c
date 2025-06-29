277adb29080141ab43c0b84c371fac29f834ac35
justNeto <49221536+justNeto@users.noreply.github.com>
2025-06-28 23:08:51 -0600

    Update keymap.c

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -45,13 +45,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_ergodox_pretty(
   TO(0),        KC_1,       KC_2,       KC_3,   KC_4,    KC_5,    KC_PSCR,              KC_MUTE,    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_NO,
   KC_DEL,       KC_SCLN,    KC_COMM,    KC_DOT, KC_P,    KC_Y,    KC_VOLU,              KC_VOLD,    KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_TILDE,
-  TG(4),        KC_A,       KC_O,       KC_E,   KC_U,    KC_I,                                      KC_D,   KC_H,   KC_T,   KC_N,   KC_S,   CW_TOGG,
+  TG(5),        KC_A,       KC_O,       KC_E,   KC_U,    KC_I,                                      KC_D,   KC_H,   KC_T,   KC_N,   KC_S,   CW_TOGG,
   KC_LSFT,      KC_QUOTE,   KC_Q,       KC_J,   KC_K,    KC_X,    KC_EXLM,              KC_QUES,    KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,   KC_RSFT,
-  KC_NO,        KC_NO,      KC_NO,      TG(2),  TG(1),                                              KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+  KC_NO,        KC_NO,      TG(3),      TG(2),  TG(1),                                              KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
 
                                                 KC_NO,  KC_NO,                  KC_NO, KC_NO,
                                                         KC_NO,                  KC_NO,
-                        LGUI_T(KC_ESC), LT(3, KC_SPC),  LCTL_T(KC_TAB),         TD(ALT_TMUX), KC_BSPC, KC_ENT
+                        LGUI_T(KC_ESC), LT(4, KC_SPC),  LCTL_T(KC_TAB),         TD(ALT_TMUX), KC_BSPC, KC_ENT
 ),
 
 /*
@@ -78,14 +78,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [1] = LAYOUT_ergodox_pretty(
   KC_TRNS,      KC_1,       KC_2,       KC_3,   KC_4,    KC_5,    KC_P,             KC_NO,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-  KC_TAB,       KC_Q,       KC_W,       KC_E,   KC_R,    KC_T,    KC_TRNS,          KC_TRNS,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_TRNS,
-  KC_ESC,       KC_A,       KC_S,       KC_D,   KC_F,    KC_G,                                  KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_TRNS,
-  KC_LSFT,      KC_Z,       KC_X,       KC_C,   KC_V,    KC_B,    KC_TRNS,          KC_TRNS,    KC_N,       KC_M,       KC_COMMA,   KC_DOT,     KC_SCLN,    KC_TRNS,
+  KC_DEL,       KC_SCLN,    KC_COMM,    KC_DOT, KC_P,    KC_Y,    KC_TRNS,          KC_TRNS,    KC_F,       KC_G,       KC_C,       KC_R,       KC_L,       KC_TRNS,
+  KC_ESC,       KC_A,       KC_0,       KC_E,   KC_U,    KC_I,                                  KC_D,       KC_H,       KC_T,       KC_N,       KC_S,       KC_TRNS,
+  KC_LSFT,      KC_QUOTE,   KC_Q,       KC_J,   KC_K,    KC_X,    KC_TRNS,          KC_TRNS,    KC_B,       KC_M,       KC_W,       KC_V,       KC_Z,       KC_TRNS,
   KC_NO,        KC_NO,      KC_NO,      KC_NO,  KC_TRNS,                                        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
 
                                                 KC_NO,  KC_NO,                  KC_NO, KC_NO,
                                                         KC_NO,                  KC_NO,
-                        LALT_T(KC_A),   LT(4, KC_SPC),  KC_LCTL,                KC_NO, KC_BSPC, KC_ENT
+                        LALT_T(KC_R),   LT(5, KC_SPC),  KC_LCTL,                KC_NO, KC_BSPC, KC_ENT
 ),
 /* L2: General Gaming
  *
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     KC_SPC,    KC_LALT, KC_LCTL,                    KC_NO, KC_BSPC, KC_ENT
 ),
 
-/* L3: Special Symbols
+/* L3: Star Citizen
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
@@ -142,18 +142,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
 [3] = LAYOUT_ergodox_pretty(
+  KC_TRNS,      KC_1,       KC_2,       KC_3,       KC_4,    KC_5,    KC_P,             KC_NO,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+  KC_TAB,       KC_Q,       KC_W,       KC_E,       KC_R,    KC_T,    KC_TRNS,          KC_TRNS,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_TRNS,
+  KC_ESC,       KC_A,       KC_S,       KC_D,       KC_F,    KC_G,                                  KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_TRNS,
+  KC_LSFT,      KC_Z,       KC_X,       KC_C,       KC_V,    KC_B,    KC_M,             KC_TRNS,    KC_N,       KC_M,       KC_COMMA,   KC_DOT,     KC_SCLN,    KC_TRNS,
+  KC_NO,        KC_NO,      KC_TRNS,    KC_NO,      OSL(5),                                          KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+
+                                                KC_NO,  KC_NO,                      KC_NO, KC_NO,
+                                                        KC_NO,                      KC_NO,
+                                    KC_SPC,    KC_LCTL, KC_LALT,                    KC_NO, KC_BSPC, KC_ENT
+),
+
+/* L4: Special Symbols
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |  Play  |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |Brwser|
+ *                                 |      |      |------|       |------|      |Back  |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+[4] = LAYOUT_ergodox_pretty(
   KC_TRNS,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,    KC_TRNS,
   KC_NO,        KC_NO,      KC_ASTR,    KC_AMPR,    KC_LBRC,    KC_LABK,    KC_NO,                  KC_NO,      KC_RABK,    KC_RBRC,    KC_PIPE,   KC_CIRC,     KC_NO,      KC_NO,
   KC_0,         KC_COLN,    KC_BSLS,    KC_LPRN,    KC_LCBR,    KC_PLUS,                                        KC_EQUAL,   KC_RCBR,    KC_RPRN,   KC_SLASH,    KC_SCLN,    KC_DLR,
   KC_TRNS,      KC_DQT,     KC_HASH,    KC_MINUS,   KC_AT,      KC_TILDE,   KC_NO,                  KC_NO,      KC_GRV,     KC_PSTE,    KC_UNDS,   KC_PERC,     KC_QUOT,    KC_NO,
-                KC_NO,        KC_NO,      KC_NO,    KC_NO,      KC_NO,                                          KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+                KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                                          KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
 
                                                     KC_NO,  KC_NO,                                                  KC_NO,  KC_NO,
                                                             KC_NO,                                                  KC_NO,
                                             KC_NO,  KC_NO,  KC_NO,                                                  KC_NO, KC_NO, KC_NO
 ),
 
-[4] = LAYOUT_ergodox_pretty(
+/* L5: Special Symbols */
+[5] = LAYOUT_ergodox_pretty(
   KC_TRNS,      KC_NO,  KC_NO,  KC_NO,      KC_NO,      KC_NO,      KC_PSCR,            KC_NO,  KC_NO,      KC_NO,  KC_NO,      KC_NO,  KC_NO,      KC_NO,
   KC_NO,        KC_F1,  KC_F2,  KC_F3,      KC_F4,      KC_F5,      KC_NO,              KC_NO,  KC_F6,      KC_F7,  KC_F8,      KC_F9,  KC_F10,     KC_NO,
   KC_TRNS,      KC_1,   KC_2,   KC_3,       KC_4,       KC_5,                                   KC_6,       KC_7,   KC_8,       KC_9,   KC_0,       KC_NO,
