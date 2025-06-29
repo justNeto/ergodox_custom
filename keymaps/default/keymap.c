b330d108fdb7ee635e0728f39669c910b1b31cbd
justNeto <49221536+justNeto@users.noreply.github.com>
2025-06-28 23:23:40 -0600

    Update keymap.c

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [1] = LAYOUT_ergodox_pretty(
   KC_TRNS,      KC_1,       KC_2,       KC_3,   KC_4,    KC_5,    KC_P,             KC_NO,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-  KC_DEL,       KC_SCLN,    KC_COMM,    KC_DOT, KC_P,    KC_Y,    KC_TRNS,          KC_TRNS,    KC_F,       KC_G,       KC_C,       KC_R,       KC_L,       KC_TRNS,
+  KC_TAB,       KC_SCLN,    KC_COMM,    KC_DOT, KC_P,    KC_Y,    KC_TRNS,          KC_TRNS,    KC_F,       KC_G,       KC_C,       KC_R,       KC_L,       KC_TRNS,
   KC_ESC,       KC_A,       KC_O,       KC_E,   KC_U,    KC_I,                                  KC_D,       KC_H,       KC_T,       KC_N,       KC_S,       KC_TRNS,
   KC_LSFT,      KC_QUOTE,   KC_Q,       KC_J,   KC_K,    KC_X,    KC_TRNS,          KC_TRNS,    KC_B,       KC_M,       KC_W,       KC_V,       KC_Z,       KC_TRNS,
   KC_NO,        KC_NO,      KC_NO,      KC_NO,  KC_TRNS,                                        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
